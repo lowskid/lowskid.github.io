5fa8669e39a2a049db6fd653be2baf86aab6fa96
Daniel <78677679+lowskid@users.noreply.github.com>
2024-05-09 06:25:36 +0000

    Delete about.js

--- about.js
+++
@@ -1,9 +0,0 @@
-function copyDiscord() {
-  // Get text field.
-  var copyText = document.getElementById("discordID");
-  // Select text field.
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  // Copy text.
-  navigator.clipboard.writeText(copyText.value);
-}
